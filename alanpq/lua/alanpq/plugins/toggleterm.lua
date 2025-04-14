f21c22fdacc6ca7f25b903dd4cd55b78c69acd5a
Alan Panayotov <alanpanayotov@gmail.com>
2025-04-14 18:44:17 +0100

    feat: add floating terminal keybind

--- alanpq/lua/alanpq/plugins/toggleterm.lua
+++ alanpq/lua/alanpq/plugins/toggleterm.lua
@@ -21,3 +21,26 @@ end
 
 vim.keymap.set({"n", "t"}, "<A-k>", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
 
+local floating = Terminal:new({
+    hidden = true,
+  direction = "float",
+  persist_size = false,
+  float_opts = {
+    border = "curved",
+    width = function ()
+      return vim.o.columns
+    end,
+    height = function ()
+      return vim.o.lines - 3
+    end,
+  },
+});
+
+function _floating_toggle()
+  floating:toggle()
+end
+WK.add({
+    mode = {"n", "t"},
+    "<A-i>", "<cmd>lua _floating_toggle()<CR>", {noremap = true, silent = true}
+})
+
